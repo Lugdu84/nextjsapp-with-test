e19698eebbd7a17b9433dd946dfb2c73f9d01411
David Grammatico <david.lugdunum@gmail.com>
2023-11-25 14:06:30 +0100

    add fizzbuzz test

---
+++ __tests__/fizzbuz.test.ts
@@ -0,0 +1,11 @@
+import { it, describe, expect } from "vitest"
+
+// returns the number if not divisible by 3 or 5
+// returns Fizz if divisible by 3
+// returns Buzz if divisible by 5
+// returns FizzBuzz if divisible by 3 and 5
+
+
+describe("Fizzbuzz by TDD", () => {
+  // implements the test cases
+})
